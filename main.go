7bd8a8173f94c2f6e80cf83dca80bddb4ee5c233
Daus Hirana <55246175+Hira478@users.noreply.github.com>
2024-03-18 11:21:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type Status struct {
 }
 
 func main() {
-	// Start the goroutine to update the JSON file every 15 seconds
+	// Mulai goroutine untuk memperbarui file JSON setiap 15 detik
 	go func() {
 		for {
 			updateJSON()
@@ -24,11 +24,12 @@ func main() {
 		}
 	}()
 
-	// Serve the web page
+	// Layani halaman web
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/index.html")
 	})
 
+	// Layani file JSON status
 	http.HandleFunc("/status.json", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "status.json")
 	})
@@ -38,17 +39,20 @@ func main() {
 }
 
 func updateJSON() {
+	// Buat struktur Status dengan nilai acak untuk air dan angin
 	status := Status{
 		Water: rand.Intn(100) + 1,
 		Wind:  rand.Intn(100) + 1,
 	}
 
+	// Marshal status ke format JSON dengan indentasi
 	statusJSON, err := json.MarshalIndent(status, "", "    ")
 	if err != nil {
 		fmt.Println("Error marshaling JSON:", err)
 		return
 	}
 
+	// Buat atau buka file status.json untuk ditulis
 	file, err := os.Create("status.json")
 	if err != nil {
 		fmt.Println("Error creating file:", err)
@@ -56,6 +60,7 @@ func updateJSON() {
 	}
 	defer file.Close()
 
+	// Tulis status JSON ke file
 	_, err = io.WriteString(file, string(statusJSON))
 	if err != nil {
 		fmt.Println("Error writing to file:", err)
