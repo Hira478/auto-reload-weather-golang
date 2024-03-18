1f5bce153d64d92fa45473340247659288767da4
Daus Hirana <55246175+Hira478@users.noreply.github.com>
2024-03-18 11:29:25 +0700

    Update main.go

--- main.go
+++ main.go
@@ -24,12 +24,12 @@ func main() {
 		}
 	}()
 
-	// Layani halaman web
+	// Melayani halaman web html
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/index.html")
 	})
 
-	// Layani file JSON status
+	// Melayani file JSON status
 	http.HandleFunc("/status.json", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "status.json")
 	})
@@ -39,20 +39,20 @@ func main() {
 }
 
 func updateJSON() {
-	// Buat struktur Status dengan nilai acak untuk air dan angin
+	// Membuat struktur Status dengan nilai acak untuk air dan angin
 	status := Status{
 		Water: rand.Intn(100) + 1,
 		Wind:  rand.Intn(100) + 1,
 	}
 
-	// Marshal status ke format JSON dengan indentasi
+	// Ubah status menjadi format JSON dengan indentasi
 	statusJSON, err := json.MarshalIndent(status, "", "    ")
 	if err != nil {
 		fmt.Println("Error marshaling JSON:", err)
 		return
 	}
 
-	// Buat atau buka file status.json untuk ditulis
+	// Membuat atau buka file status.json untuk ditulis
 	file, err := os.Create("status.json")
 	if err != nil {
 		fmt.Println("Error creating file:", err)
@@ -60,7 +60,7 @@ func updateJSON() {
 	}
 	defer file.Close()
 
-	// Tulis status JSON ke file
+	// Menulis status JSON ke file
 	_, err = io.WriteString(file, string(statusJSON))
 	if err != nil {
 		fmt.Println("Error writing to file:", err)
